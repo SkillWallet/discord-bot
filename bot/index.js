322aaedcdd0a5a65f0f5b12e06b9fc10ec2934aa
Milena Monova <mimonova13@gmail.com>
2021-11-26 14:54:01 +0200

    polls implemented

--- bot/index.js
+++ bot/index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 // imports
-const { Client, MessageEmbed } = require('discord.js');
+const { Client, RichEmbed } = require('discord.js');
 const { VoiceChannelsDetailsStorage } = require('./voiceEventsStorage')
 const { getCommunityDetails } = require('./api')
+const getEmojis = require('discordjs-getemojis');
 
 // constants
 const ROLE_COLORS = ['BLUE', 'GREEN', 'PURPLE'];
 const TOKEN = process.env.TOKEN;
-
 
 // init
 const bot = new Client();
@@ -66,20 +66,62 @@ bot.on('message', async msg => {
     console.log(channelID.id);
   }
 
-  if (msg.content === '/post-poll') {
+  if (msg.content.startsWith('/post-poll')) {
 
-    // console.log(a);
+    // const channel = await msg.guild.channels.cache.get(channelSettings.channelID);
     const polls = getPolls();
-    const channelSettings = getChannel(undefined);
-
-    const channel = await msg.guild.channels.cache.get(channelSettings.channelID);
 
     polls.forEach(async poll => {
-      const pollContent = new MessageEmbed().setTitle(poll.title).setDescription(`${poll.description}\nThis poll expires on ${poll.endDate}`).setColor('PINK');
-      const message = await channel.send({ embed: pollContent });
-      console.log(message.id);
-      // store messageID <-> pollID in a database!
-    });
+
+      const description = msg.content.replace('/post-poll ', '');
+
+      const emojis = getEmojis(msg);
+
+      const pollContent = new RichEmbed().setTitle(poll.title).setDescription(`${description}\nThis poll expires on ${poll.endDate}`);
+
+      msg.channel.send({ embed: pollContent }) // Use a 2d array?
+        .then(async function (message) {
+          var reactionArray = [];
+          for (var i = 0; i < emojis.length; i++) {
+            reactionArray[i] = await message.react(emojis[i] === 'object' ? emojis[i].id : emojis[i]);
+          }
+
+          setTimeout(() => {
+            // Re-fetch the message and get reaction counts
+            message.channel.fetchMessage(message.id)
+              .then(async function (message) {
+                var reactionCountsArray = [];
+                for (var i = 0; i < reactionArray.length; i++) {
+                  console.log('reactions', i, reactionArray[i].count - 1);
+                  reactionCountsArray[i] = reactionArray[i].count - 1;
+                }
+
+                // Find winner(s)
+                var max = -Infinity, indexMax = [];
+                for (var i = 0; i < reactionCountsArray.length; ++i)
+                  if (reactionCountsArray[i] > max) max = reactionCountsArray[i], indexMax = [i];
+                  else if (reactionCountsArray[i] === max) indexMax.push(i);
+
+                console.log('indexMax', indexMax)
+                // Display winner(s)
+                var winnersText = "";
+                if (reactionCountsArray[indexMax[0]] == 0) {
+                  winnersText = "No one voted!"
+                } else {
+                  for (var i = 0; i < indexMax.length; i++) {
+                    console.log('winner emoji', i, emojis[i]);
+                    winnersText +=
+                      emojis[indexMax[i]] + " (" + reactionCountsArray[indexMax[i]] + " vote(s))\n";
+                  }
+                }
+                pollContent.addField("**Winner(s):**", winnersText);
+                pollContent.setFooter(`The vote is now closed!`);
+                pollContent.setTimestamp();
+                msg.channel.send({ embed: pollContent })
+              });
+          }, new Date(poll.endDate) - Date.now() > 0 ? new Date(poll.endDate) - Date.now() : 10000);
+        }).catch(console.error);
+    })
   }
   if (msg.content === '/connect-sw') {
     msg.reply(`Please follow this link https://discord.com/api/oauth2/authorize?client_id=898586559228551208&redirect_uri=http%3A%2F%2Flocalhost%3A3334&response_type=code&scope=identify`);
@@ -100,21 +142,20 @@ function getPolls() {
     {
       tokenId: 1,
       title: 'Is SkillWallet awesome?',
-      description: 'SkillWallet is super awesome, what do you think?',
-      endDate: '2021-11-19 18:03:00'
+      endDate: '2021-11-25 14:22:00',
     },
-    {
-      tokenId: 2,
-      title: 'Is Milena awesome?',
-      description: 'Milena is super awesome, what do you think?',
-      endDate: '21-Nov-2021',
-    },
-    {
-      tokenId: 3,
-      title: 'Is DiTo awesome?',
-      description: 'DiTo is super awesome, what do you think?',
-      endDate: '22-Nov-2021',
-    }
+    // {
+    //   tokenId: 2,
+    //   title: 'Is Milena awesome?',
+    //   description: 'Milena is super awesome, what do you think?',
+    //   endDate: '21-Nov-2021',
+    // },
+    // {
+    //   tokenId: 3,
+    //   title: 'Is DiTo awesome?',
+    //   description: 'DiTo is super awesome, what do you think?',
+    //   endDate: '22-Nov-2021',
+    // }
   ]
 }
 
