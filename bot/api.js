aa4f34d1b63c56072583462386d06cd649ada423
Milena Monova <mimonova13@gmail.com>
2022-02-09 19:47:35 +0200

    removed unused GET request

--- bot/api.js
+++ bot/api.js
@@ -1,14 +1,9 @@
 
 const axios = require('axios');
 
-const getDiscordConnectNonce = async () => {
-    // TODO: change action!
-    const resp = await axios.post(`https://api.skillwallet.id/api/skillwallet/-1/nonces?action=1`);
-    return resp.data;
-}
 const getCommunityDetails = async (key) => {
-    const resp = await axios.get(`https://api.distributed.town/api/community/key/${key}`);
+    const resp = await axios.get(`https://dev-api.distributed.town/api/community/key/${key}`);
     return resp.data;
 }
 
-module.exports = { getDiscordConnectNonce, getCommunityDetails }
+module.exports = { getCommunityDetails }
